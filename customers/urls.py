53e02b81c65ba1ab894e7d41d27dba009c92c30b
tanvir43 <raihncse43@gmail.com>
2022-09-20 11:46:05 +0600

    rendering customers home html page

--- customers/urls.py
+++ customers/urls.py
@@ -1,5 +1,6 @@
-# from django.urls import path
+from django.urls import path
+from .views import customers
 
-# urlpatterns = [
-#     path('',),
-# ]
+urlpatterns = [
+    path('', customers, name='customers'),
+]
